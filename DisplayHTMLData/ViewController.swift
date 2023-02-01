49c824a522aae44c299972f9772254c90f13f658
harundemirkaya <harundmrk@gmail.com>
2023-02-01 11:03:30 +0300

    Initial Commit

---
+++ DisplayHTMLData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisplayHTMLData
+//
+//  Created by Harun Demirkaya on 1.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
